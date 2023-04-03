6d07f5790d8e4d1298a91ae3367fba970924bf5b
johnmerga <johnmerga@yahoo.com>
2023-04-04 02:15:14 +0300

    Implemented CORS and Helmet to improve security measures

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Express, NextFunction,Request,Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import { morganMiddleware } from './logger';
-import { UserRouter,AuthRouter } from './router';
+import { UserRouter, AuthRouter } from './router';
 
 import { ApiError, errorConverter, errorHandler } from "./errors"
 import httpStatus from 'http-status';
@@ -17,18 +19,18 @@ import httpStatus from 'http-status';
 class App {
     public app: Express;
     //routes
-    
+
 
     constructor() {
         this.app = express();
         this.config();
         //auth routes
-        this.app.use('/api/v1/auth', new AuthRouter().routes())
+        this.app.use('/api/v1/auth', new AuthRouter().routes());
         // user routes
         this.app.use('/api/v1/users', new UserRouter().routes());
 
         // unknown route
-        this.app.use((req:Request, res:Response, next:NextFunction) => {
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
             next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
         });
         // error converter
@@ -38,10 +40,15 @@ class App {
     }
 
     private config() {
+        // set security HTTP headers
+        this.app.use(helmet());
+        // enable cors
+        this.app.use(cors());
+        this.app.use('*', cors())
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: false }));
         this.app.use(morganMiddleware);
-        
+
     }
 
 
